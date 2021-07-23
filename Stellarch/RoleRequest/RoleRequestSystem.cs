d92dff8cdaa2564f8f250b6b8cfb31c7c8686c46
Alex Boritz <alex.boritz@gmail.com>
2021-07-23 17:34:06 -0400

    Role request short circuit

--- Stellarch/RoleRequest/RoleRequestSystem.cs
+++ Stellarch/RoleRequest/RoleRequestSystem.cs
@@ -248,6 +248,12 @@ namespace BigSister.RoleRequest
                                              DiscordMessage message,
                                              DiscordEmoji emoji)
         {
+            //short circuit, don't even try if the message isn't floppys 
+            if (!message.Author.IsCurrent)
+            {
+                return;
+            }
+
             var callingMember = await guild.GetMemberAsync(user.Id);
 
             // Make sure the person is a colonist and not muted
